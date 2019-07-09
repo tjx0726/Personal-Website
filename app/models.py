5ce32ba784798b2f066876b6dac19dd664e7b051
tjx0726 <tjx0726@gmail.com>
2019-07-09 16:59:52 +0100

    registration done

--- app/models.py
+++ app/models.py
@@ -1,20 +1,40 @@
-from app import db
+from app import db, login
+#Flask-Login user mixin class
+from flask_login import UserMixin
+#timestamp
 from datetime import datetime
+#password validation - hashing
+from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
+    firstname = db.Column(db.String(64), index=True)
+    lastname = db.Column(db.String(64), index=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    #one-many relationship. put relationship on the "one" side
+    #u.posts.all() can be used to query all posts by a user
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)    
+        return '<User {}, {} {}>'.format(self.username, self.firstname, self.lastname)    
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    #foreign key referenced to user.id
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
--- app/test.py
+++
@@ -1,3 +0,0 @@
-from models import User
-u = User(username='susan', email='susan@example.com')
-print(u)

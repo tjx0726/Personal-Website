61cbc1c4e5a242a8a294cd2db4cb9e5899de64cd
tjx0726 <tjx0726@gmail.com>
2020-03-06 13:57:25 -0500

    new flask start

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'amaqykpts'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+        'mysql://tonytong:950726@localhost/test'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
